78c8e94d92c5a986d09c42385b2cc9aa5fd03df4
VALECD3 <deepak26v@gmail.com>
2017-04-21 13:59:01 -0400

    Code Updates

--- src/test/java/com/boot/DasBootApplicationTests.java
+++ src/test/java/com/boot/DasBootApplicationTests.java
@@ -1,16 +1,22 @@
 package com.boot;
 
+import com.boot.controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DasBootApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testHomeContoller() {
+		HomeController homeController = new HomeController();
+		String result = homeController.home();
+		assertEquals(result, "Das Boot, reporting for duty !!");
 	}
 
 }
---
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -0,0 +1,43 @@
+package com.boot;
+
+import com.boot.controller.ShipwreckController;
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ShipwreckControllerTest {
+
+    @InjectMocks
+    private ShipwreckController shipwreckController;
+
+    @Mock
+    private ShipwreckRepository shipwreckRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testShipwreckGet() {
+        Shipwreck shipwreck = new Shipwreck();
+        shipwreck.setId(1L);
+        when(shipwreckRepository.findOne(1L)).thenReturn(shipwreck);
+
+        Shipwreck sw = shipwreckController.get(1L);
+        verify(shipwreckRepository).findOne(1L);
+        assertEquals(1L, sw.getId().longValue());
+    }
+}
